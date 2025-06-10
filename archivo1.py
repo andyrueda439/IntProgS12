50b0efc5836cad13d9c5672c8f4cca1c5ea0df22
andyrueda439 <ajrueda@uamv.edu.ni>
2025-06-10 09:29:18 -0500

    Probando archivo

---
+++ archivo1.py
@@ -0,0 +1,8 @@
+try: 
+    mi_ruta = 'C:\\Users\\Dell\\Documents\\archivo\\'
+    mi_archivo = open(mi_ruta + 'datos.txt', 'r')
+    contenido = mi_archivo.read()
+    print(contenido)
+    mi_archivo.close()
+except FileNotFoundError:
+    print("El archivo no se encuentra en la ruta especificada.")
